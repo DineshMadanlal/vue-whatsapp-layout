b6bc021700b16b61493130b9136796aa27a0d2a5
Dinesh Madanlal <>
2021-06-12 17:07:36 +0530

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Whatsapp layout -->
+  <q-layout view="lhh lpR lff">
+    <!-- Left side drawer -->
+    <q-drawer
+      v-model="leftDrawerOpen"
+      overlay
+      bordered
+      side="left"
+      behavior="desktop"
+    >
+      <!-- drawer content -->
+    </q-drawer>
+
+    <!-- Right side content -->
+    <q-page-container>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false // state variable to open/close the sidebar
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
